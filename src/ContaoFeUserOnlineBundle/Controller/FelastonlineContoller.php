cd52bd17ea2408c4e020e8427e72c875b3cbd5c8
hh-com <info@harald-huber.com>
2019-02-14 00:43:30 +0000

    01:43

--- src/ContaoFeUserOnlineBundle/Controller/FelastonlineContoller.php
+++ src/ContaoFeUserOnlineBundle/Controller/FelastonlineContoller.php
@@ -3,7 +3,7 @@
 #declare(strict_types=1);
 
 /*
- * This file is part of the FacebookLogin Bundle.
+ * This file is part of the Fronten User Last Online Bundle.
  *
  * (c) inspiredminds
  *
@@ -30,58 +30,17 @@ use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
 
 
-class FelastonlineContoller extends Controller
+class FelastonlineController extends Controller
 {
 
     public function __invoke($action = 1, Request $request)
     {     
-        
-        $return = array();
+        return 33;
 
-        $data = $request->request->get('request');
+        return new JsonResponse( array('status' => false ) ); 
 
-		return $data;
-        #$this->container->get('contao.framework')->initialize();  
 
         
-
-        $objUser = \Contao\FrontendUser::getInstance();
-        if ($objUser->id != null)
-        {
-            
-            /**
-             * Nur ausführen wenn usr numerisch ist!!! 
-             */
-            if ( is_numeric(\Input::post('usr')) )
-            {
-                $wantToFollowId = \Input::post('usr');
-            }
-            else
-            {
-                \ArtistHooks::adminAlertMail(__CLASS__, 
-                    "!ACHTUNG Follower ID ist nicht numerisch! 
-                    Angriffsversuch/Fehler? 
-                    UserId: ".$objUser->id." wantToFollowId: ".\Input::post('usr')."" );
-
-                return new JsonResponse( array('status' => false ) ); 
-            } 
-            /** zur tl_member_foloow hinzufügen */
-            $addedStatus = \ArtistHooks::addIFollowYou($wantToFollowId);
-
-            $return[] = array(
-                'status'=> $addedStatus
-                );
-
-            #return new JsonResponse(["Please login"]); 
-        }
-        else {
-            # User ist nicht eingeloggt.
-            $return[] = array(
-                'status'=>'loggedout'
-            );
-        }
-
-        return new JsonResponse($return); 
 
         throw new NotFoundHttpException("Page not found"); 
     }
--- src/ContaoFeUserOnlineBundle/Resources/contao/modules/ModuleFeUserOnline.php
+++ src/ContaoFeUserOnlineBundle/Resources/contao/modules/ModuleFeUserOnline.php
@@ -1,30 +1,20 @@
 <?php
 
-namespace ContaoMessengerBundle;
+namespace ContaoFeUserOnlineBundle;
 
-class ModuleMessenger extends \ModuleMessenger
+class ModuleFeUserOnline extends \System
 {
-    /**
-	 * Template
-	 * @var string
-	 */
-	protected $strTemplate = 'mod_messenger';
+    
+	protected $strTemplate = 'js_feuseronline';
+	
 	/**
-	 * Display a wildcard in the back end
+	 * Display a wildcard in the back end 
 	 *
 	 * @return string
 	 */
-	public function generate()
+	public static function test()
 	{
-		if (TL_MODE == 'BE')
-		{
-			/** @var \BackendTemplate|object $objTemplate */
-			$objTemplate = new \BackendTemplate('be_wildcard');
-			$objTemplate->wildcard = '### ' . utf8_strtoupper($GLOBALS['TL_LANG']['FMD']['breadcrumb'][0]) . ' ###';
-			$objTemplate->title = "Contao Messenger";
-			return $objTemplate->parse();
-		}
-		return parent::generate();
+		echo "haaallllo";
 	}
 
 	    
@@ -33,7 +23,7 @@ class ModuleMessenger extends \ModuleMessenger
      */
     protected function compile()
     {
-		echo 222222222222222222222222222222;
+		echo "compile  FEUSERONLINEEEEEEE";
 		echo "verwende das andere file -.......";
 		exit;
 		if (FE_USER_LOGGED_IN !== true) 
@@ -41,39 +31,9 @@ class ModuleMessenger extends \ModuleMessenger
 			die("Please log in!");
 		}
 
-		$user = \Contao\FrontendUser::getInstance();
-
-
-
-
-		$usrDrop = $this->getAllUsersOrFollowers($user, true);
-		
-        $this->Template->userDropdown = $this->getAllUsersOrFollowers($user, true);
 	}
 
+	
 
-	public function getAllUsersOrFollowers($currentLoggedin, $allUsers = false)
-	{
-
-		if ($allUsers == false)
-		{
-			$objMem = \Database::getInstance()
-			->prepare("SELECT * FROM tl_member_follow WHERE id != ? or followThis = ? ")
-			-> limit(50)
-			->execute( $currentLoggedin->id, $currentLoggedin->uniqueID )
-			->fetchAllAssoc();
-		}
-		else{
-
-			$objMem = \Database::getInstance()
-			->prepare("SELECT * FROM tl_member WHERE id != ? AND disable = ? ")
-			-> limit(50)
-			->execute( $currentLoggedin->id, "" )
-			->fetchAllAssoc();
-		}
-
-		return $objMem;
-
-	}
 	
 }
